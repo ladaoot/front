b47c893c5d1a353baee15d6b3da99e75dc5abf84
ladacumakova5 <ladachumakova5@gmail.com>
2024-07-02 14:03:01 +0300

    add categories to filter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Pages } from './pages/index.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
+const theme = {
+  styles: {
+    global: (props) => ({
+      'html, body': {
+        fontSize: 'sm',
+        color: 'dark' ,
+        lineHeight: 'tall',
+      },
+      a: {
+        color: props.colorMode === 'dark' ? 'teal.300' : 'teal.500',
+      },
+    }),
+  },
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode  >
+    <ChakraProvider  theme={theme}>
+      <Pages />
+    </ChakraProvider>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
